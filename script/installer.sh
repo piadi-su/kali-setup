31abcf39312a777a8f2e104fe0b3ed12996dd39a
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-04-25 23:22:06 +0200

    fixing installer.sh

--- script/installer.sh
+++ script/installer.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
+set -euo pipefail
 
-#update
+echo "Updating system..."
 sudo apt update && sudo apt upgrade -y
 
-#install pkg's
-sudo apt install git tmux neovim kitty i3 i3blocks zoxide kitty rofi terminator fastfetch dunst nautilus -y
+echo "Installing packages..."
+sudo apt install -y \
+  git tmux neovim kitty i3 i3blocks zoxide rofi terminator \
+  fastfetch dunst nautilus build-essential curl unzip
 
-#cloning the repo
-https://github.com/piadi-su/kali-setup.git
+echo "Cloning dotfiles repo..."
+git clone https://github.com/piadi-su/kali-setup.git
 
+echo "Installing Oh My Zsh..."
+RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ZSH CUSTOM PATH
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+echo "Installing Zsh plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions.git \
+  "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git \
+  "$ZSH_CUSTOM/themes/powerlevel10k"
 
-#nvim config
-https://github.com/piadi-su/my_nvim.git $HOME/.config/nvim
+echo "Installing Neovim config..."
+git clone https://github.com/piadi-su/my_nvim.git "$HOME/.config/nvim"
 
-#
-cd kali-setup/dotfiles
+echo "Applying dotfiles..."
 
-mv i3  $HOME/.config
-mv i3blocks  $HOME/.config
-mv kitty $HOME/.config
-mv rofi $HOME/.config
-mv .tmux.conf $HOME
-mv .zshrc $HOME
+cd "$HOME/kali-setup/dotfiles"
 
-mv wallpaper $HOME
+# directories
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.local/bin"
 
-cd $HOME
+# configs
+cp -r i3 "$HOME/.config/"
+cp -r i3blocks "$HOME/.config/"
+cp -r kitty "$HOME/.config/"
+cp -r rofi "$HOME/.config/"
 
-#autotiling
+cp .tmux.conf "$HOME/"
+cp .zshrc "$HOME/"
+
+cp -r wallpaper "$HOME/"
+
+echo "Installing autotiling..."
+
 git clone https://github.com/piadi-sudo/i3-autotiling-in-c.git
 cd i3-autotiling-in-c
 make
-mv autotiling ~/.local/bin/
 
+mv autotiling "$HOME/.local/bin/"
 
+echo "Setup completed!"
